f92f57f6de8878a91ce2ed1b49d15b8413955740
Ezio-33 <8691@holbertonstudents.com>
2024-06-10 15:13:12 +0200

    correction du fichier avis avec Jeanalexandre et ajout test de classe

--- classes/avis.py
+++ classes/avis.py
@@ -6,17 +6,18 @@ Module contenant la classe Avis.
 import uuid
 from datetime import datetime
 from utilisateur import Utilisateur
+from lieu import Lieu
 
 class Avis:
     """
     Classe représentant un avis.
     """
-    def __init__(self, commentaire: str, note: int, utilisateur: Utilisateur, lieu_id: uuid.UUID):
+    def __init__(self, commentaire: str, note: int, utilisateur: Utilisateur, lieu: Lieu):
         self.id = uuid.uuid4()
         self.commentaire = commentaire
         self.note = note
         self.utilisateur_id = utilisateur.id
-        self.lieu_id = lieu_id
+        self.lieu_id = lieu.id
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
